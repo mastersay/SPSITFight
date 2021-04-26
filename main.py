2fa520e5c0952796a9d4ecd5ab6e816f2deb5a5d
master <japrusko@gmail.com>
2021-04-26 18:22:02 +0200

    v000.000.006 Screen Manager # Conflicts: #	README.md #	main.py

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
-from functools import partial
-from kivy.uix.widget import Widget
-from kivy.uix.screenmanager import *
+# from functools import partial
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import ObjectProperty
+from kivy.lang import Builder
 from random import randint, choice
 
 
@@ -73,8 +69,8 @@ class BossHero:
         return attack_pick
 
 
-class OpenScreen(Widget):
-    name = ObjectProperty(None)
+class OpenScreen(Screen):
+    playerName = ObjectProperty(None)
     submit_button = ObjectProperty(None)
 
     def __init__(self, **kwargs):
@@ -82,9 +78,9 @@ class OpenScreen(Widget):
         super(OpenScreen, self).__init__(**kwargs)
 
     def submit_btn(self):
-        if self.name.text:
-            if len(self.name.text) < 18:
-                self.main_hero.nick = self.name.text
+        if self.playerName.text:
+            if len(self.playerName.text) < 18:
+                self.main_hero.nick = self.playerName.text
                 print(self.main_hero.nick)
                 return True
             else:
@@ -95,51 +91,25 @@ class OpenScreen(Widget):
         return False
 
 
-class Style(GridLayout):
-    def add_hero_clean(self, instance, text_box, clean_text_box=True):
-        if text_box.text:
-            self.superheroes.add_superhero(text_box.text)
-            if clean_text_box:
-                text_box.text = ""
-        print(self.superheroes.super_heroes)
+class MainScreen(Screen):
+    pass
 
-    def __init__(self, **kwargs):
-        super(Style, self).__init__(**kwargs)
 
-        self.superheroes = MainHero()
+class FightScreen(Screen):
+    pass
 
-        self.cols = 1
-        self.rows = 2
 
-        self.add_superhero_grid = GridLayout()
-        self.add_superhero_grid.cols = 2
-        self.add_superhero_grid.rows = 1
+class ScreenManagement(ScreenManager):
+    pass
 
-        self.add_superhero_grid.add_widget(Label(text="Add Superhero"))
 
-        # self.name = TextInput(multiline=False)
-        self.superhero_name = TextInput(multiline=False)
-        self.add_superhero_grid.add_widget(self.superhero_name)
-
-        self.add_widget(self.add_superhero_grid)
-
-        self.submit_new_hero_grid = GridLayout()
-        self.submit_new_hero_grid.cols = 1
-
-        self.submit_new_hero_button = Button(text="Add new Superhero", font_size=30)
-        self.submit_new_hero_button.bind(on_press=partial(self.add_hero_clean, text_box=self.superhero_name))
-        self.submit_new_hero_grid.add_widget(self.submit_new_hero_button)
-
-        self.add_widget(self.submit_new_hero_grid)
-
-        # print(self.superheroes.super_heroes)
+kv = Builder.load_file('AppDesign.kv')
 
 
 class Design(App):
     def build(self):
-        return OpenScreen()
+        return kv
 
 
 if __name__ == "__main__":
     Design().run()
-    print("APP ENDED!")
