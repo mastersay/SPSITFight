77a148b3b5bfe2d0db67cc7da6a67634e9db1fdc
master <japrusko@gmail.com>
2021-04-27 08:40:59 +0200

    v000.000.006 Screen Manager # Conflicts: #	README.md #	main.py

--- main.py
+++ main.py
@@ -1,66 +1,115 @@
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
-from functools import partial
+# from functools import partial
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import ObjectProperty
+from kivy.lang import Builder
+from random import randint, choice
 
 
-class SuperHeroes:
+# player avatar
+class MainHero:
     def __init__(self):
-        self.super_heroes = {}
+        # name of player
+        self.nick = "Player"
+        # player lvl
+        self.lvl = 1
 
-    def add_superhero(self, name):
-        if name not in self.super_heroes:
-            self.super_heroes[name] = [{'lvl': 0}, {'hp': 100}]
+        # player stats
+        # player health
+        self.health = self.lvl * 10
+        # amount of healing for health
+        self.heal = {'heal': 0, 'max_heal': self.lvl}
+
+        # fight stats
+        self.programming_stat = 1
+        self.design_stat = 1
+        self.creativity_stat = 1
 
 
-class Design(GridLayout):
-    def add_hero_clean(self, instance, text_box, clean_text_box=True):
-        if text_box.text:
-            self.superheroes.add_superhero(text_box.text)
-            if clean_text_box:
-                text_box.text = ""
-        print(self.superheroes.super_heroes)
+#  TODO: NPC class
+
+# enemy boss avatars
+class BossHero:
+    def __init__(self, name: str, lvl: int, stats: dict):
+        self.name = name
+
+        self.health = lvl * 10
+        self.programming_stat = stats['programming_stat']
+        self.design_stat = stats['design_stat']
+        self.creativity = stats['creativity_stat']
+        self.stats = stats
+
+    def attack(self):
+        stat_values = sorted(self.stats.values())
+
+        def difference_check():
+            return True if stat_values == sorted(set(stat_values)) else False
+
+        def key_by_value(dictionary, search_by):
+            if difference_check():
+                for key, value in dictionary.items():
+                    if value == search_by:
+                        return key
+            else:
+                return [key for key, value in dictionary.items() if value == search_by]
+
+        chance = randint(1, 100)
+        if difference_check():
+            if chance > 40:
+                attack_pick = key_by_value(self.stats, max(stat_values))
+            elif chance > 10:
+                attack_pick = stat_values[1]
+            else:
+                attack_pick = key_by_value(self.stats, min(stat_values))
+        else:
+            if chance > 40:
+                attack_pick = choice(key_by_value(self.stats, max(stat_values)))
+            else:
+                attack_pick = choice(key_by_value(self.stats, min(stat_values)))
+        return attack_pick
+
+
+class OpenScreen(Screen):
+    playerName = ObjectProperty(None)
+    submit_button = ObjectProperty(None)
 
     def __init__(self, **kwargs):
-        super(Design, self).__init__(**kwargs)
+        self.main_hero = MainHero()
+        super(OpenScreen, self).__init__(**kwargs)
 
-        self.superheroes = SuperHeroes()
-
-        self.cols = 1
-        self.rows = 2
-
-        self.add_superhero_grid = GridLayout()
-        self.add_superhero_grid.cols = 2
-        self.add_superhero_grid.rows = 1
-
-        self.add_superhero_grid.add_widget(Label(text="Add Superhero"))
-
-        # self.name = TextInput(multiline=False)
-        self.superhero_name = TextInput(multiline=False)
-        self.add_superhero_grid.add_widget(self.superhero_name)
-
-        self.add_widget(self.add_superhero_grid)
-
-        self.submit_new_hero_grid = GridLayout()
-        self.submit_new_hero_grid.cols = 1
-
-        self.submit_new_hero_button = Button(text="Add new Superhero", font_size=30)
-        self.submit_new_hero_button.bind(on_press=partial(self.add_hero_clean, text_box=self.superhero_name))
-        self.submit_new_hero_grid.add_widget(self.submit_new_hero_button)
-
-        self.add_widget(self.submit_new_hero_grid)
-
-        # print(self.superheroes.super_heroes)
+    def submit_btn(self):
+        if self.playerName.text:
+            if len(self.playerName.text) < 18:
+                self.main_hero.nick = self.playerName.text
+                print(self.main_hero.nick)
+                return True
+            else:
+                print("Name can't exceed 18 characters!")
+                self.submit_button.text = "Name can't exceed 18 characters!"
+                return False
+        print(self.main_hero.nick)
+        return False
 
 
-class Trying(App):
+class MainScreen(Screen):
+    pass
 
+
+class FightScreen(Screen):
+    pass
+
+
+class ScreenManagement(ScreenManager):
+    pass
+
+
+kv = Builder.load_file('AppDesign.kv')
+
+
+class Design(App):
     def build(self):
-        return Design()
+        return kv
 
 
 if __name__ == "__main__":
-    Trying().run()
-    print()
+    Design().run()
