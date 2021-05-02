80a640780e5a74b83b853ffb2adc1af28fffb28d
master <japrusko@gmail.com>
2021-05-02 15:34:41 +0200

    v000.000.007 preparing fight_screen, finished upgrade func # Conflicts: #   AppDesign.kv #	main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import ObjectProperty
 from kivy.lang import Builder
+from kivy.uix.label import Label
 from kivy.core.window import Window
 # from kivy.logger import Logger, LoggerHistory
 from kivy.clock import Clock
@@ -84,6 +85,8 @@ class OpenScreen(Screen):
 
 
 class MainScreen(Screen):
+    coins_label = ObjectProperty(None)
+
     fight_button = ObjectProperty(None)
     boss_fight_button = ObjectProperty(None)
 
@@ -103,29 +106,63 @@ class MainScreen(Screen):
         super(MainScreen, self).__init__(**kwargs)
         Clock.schedule_once(self.after_init)
 
-    # do after init
+    # Schedule after init
     def after_init(self, dt):
         self.app = App.get_running_app()
         self.programming_stat_label.text = f'Programming {self.app.main_hero.programming_stat}'
         self.design_stat_label.text = f'Design {self.app.main_hero.design_stat}'
         self.creativity_stat_label.text = f'Creativity {self.app.main_hero.creativity_stat}'
         self.heal_stat_label.text = f"Heal {self.app.main_hero.heal['heal']}/{self.app.main_hero.heal['max_heal']}"
+        self.coins_label.text = f"Coins {self.app.main_hero.coins}"
 
     # func for upgrade stats
     def upgrade(self, stat):
+        upgrade_price = 0
         if stat != "heal":
-            setattr(self.app.main_hero, stat, getattr(self.app.main_hero, stat) + 1)
-            stringstat = stat.removesuffix('_stat')
-            print(getattr(getattr(self, f'{stat}_label'), 'text'))
-            setattr(getattr(self, f'{stat}_label'), 'text',
-                    f"{stringstat[0].capitalize() + stringstat[1:]} {getattr(self.app.main_hero, stat)}")
+            if self.app.main_hero.coins >= getattr(self.app.main_hero, stat):
+                upgrade_price = getattr(self.app.main_hero, stat)
+                setattr(self.app.main_hero, stat, getattr(self.app.main_hero, stat) + 1)
+                stat_no_suffix = stat.removesuffix('_stat')
+                print(getattr(getattr(self, f'{stat}_label'), 'text'))
+                setattr(getattr(self, f'{stat}_label'), 'text',
+                        f"{stat_no_suffix[0].capitalize() + stat_no_suffix[1:]} {getattr(self.app.main_hero, stat)}")
         elif getattr(self.app.main_hero, 'heal')['heal'] < getattr(self.app.main_hero, 'heal')['max_heal']:
-            self.app.main_hero.heal['heal'] += 1
-            self.heal_stat_label.text = f"Heal {self.app.main_hero.heal['heal']}/{self.app.main_hero.heal['max_heal']}"
+            if self.app.main_hero.coins >= self.app.main_hero.heal['max_heal']:
+                upgrade_price = self.app.main_hero.heal['max_heal']
+                self.app.main_hero.heal['heal'] += 1
+                self.heal_stat_label.text = f"Heal {self.app.main_hero.heal['heal']}/{self.app.main_hero.heal['max_heal']}"
+        self.app.main_hero.coins -= upgrade_price
+        self.coins_label.text = f"Coins {self.app.main_hero.coins}"
 
 
 class FightScreen(Screen):
-    pass
+    programming_stat_label = ObjectProperty(None)
+    # programming_upgrade_btn: ObjectProperty(None)
+
+    design_stat_label = ObjectProperty(None)
+    # design_upgrade_btn: ObjectProperty(None)
+
+    creativity_stat_label = ObjectProperty(None)
+    # creativity_upgrade_btn: ObjectProperty(None)
+
+    heal_stat_label = ObjectProperty(None)
+    # heal_buy_btn: ObjectProperty(None)
+
+    enemy_programming_stat_label = ObjectProperty(None)
+    enemy_design_stat_label = ObjectProperty(None)
+    enemy_creativity_stat_label = ObjectProperty(None)
+
+    def __init__(self, **kwargs):
+        super(FightScreen, self).__init__(**kwargs)
+        Clock.schedule_once(self.after_init)
+
+    # Schedule after init
+    def after_init(self, dt):
+        self.app = App.get_running_app()
+        self.programming_stat_label.text = f'Programming {self.app.main_hero.programming_stat}'
+        self.design_stat_label.text = f'Design {self.app.main_hero.design_stat}'
+        self.creativity_stat_label.text = f'Creativity {self.app.main_hero.creativity_stat}'
+        self.heal_stat_label.text = f"Heal {self.app.main_hero.heal['heal']}"
 
 
 class ScreenManagement(ScreenManager):
@@ -143,6 +180,7 @@ class Design(App):
     def __init__(self, **kwargs):
         super(Design, self).__init__(**kwargs)
         self.main_hero = MainHero()
+        self.boss_enemies = []
 
     # construct app
     def build(self):
@@ -156,12 +194,16 @@ class Design(App):
                 print(self.main_hero.nick)
                 self.root.current = 'MainScreen'
                 self.root.ids.open_screen.player_name_txtIn.focus = False
+                self.root.ids.main_screen.add_widget(
+                    Label(text=self.main_hero.nick, pos_hint={'center_x': .5, 'center_y': .35}))
             else:
                 print("Name can't exceed 18 characters!")
                 self.root.ids.open_screen.warning_label.text = "Name can't exceed 18 characters!"
         else:
             self.root.current = 'MainScreen'
             self.root.ids.open_screen.player_name_txtIn.focus = False
+            self.root.ids.main_screen.add_widget(
+                Label(text=self.main_hero.nick, pos_hint={'center_x': .5, 'center_y': .35}))
         print(self.main_hero.nick)
 
 
